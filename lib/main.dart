7e93f538a4882b5f8589c0ee3fcd7351fbce086c
Alan Medeiros <118941629+AlanMedeirosAG@users.noreply.github.com>
2023-10-31 14:18:48 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gymapp/Telas/exercicio_tela.dart';
+import 'package:flutter_gymapp/Telas/autenticacao_tela.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:TelaExercicio()
+      home:AutenticacaoTela()
     );
   }
 }
